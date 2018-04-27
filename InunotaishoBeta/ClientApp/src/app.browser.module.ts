643abd70f6f31c3424e97b57f4bf9624aecb4d3f
inunotaisho26 <ebfairweather@gmail.com>
2018-04-27 14:08:58 -0400

    adding browser app module

--- InunotaishoBeta/ClientApp/src/app.browser.module.ts
+++ InunotaishoBeta/ClientApp/src/app.browser.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ORIGIN_URL, REQUEST } from '@nguniversal/aspnetcore-engine/tokens';
 import { AppModuleShared } from './app.shared.module';
 import { AppComponent } from './app/app.component';
+import { BrowserTransferStateModule } from '@angular/platform-browser';
+import { BrowserPrebootModule } from 'preboot/browser';
+
+export function getOriginUrl() {
+    return window.location.origin;
+  }
+  
+  export function getRequest() {
+    // the Request object only lives on the server
+    return { cookie: document.cookie };
+  }
 
 @NgModule({
     bootstrap: [ AppComponent ],
     imports: [
-        BrowserModule,
+        BrowserPrebootModule.replayEvents(),
+        BrowserAnimationsModule,
+
+        // Our Common AppModule
         AppModuleShared
     ],
     providers: [
-        { provide: 'BASE_URL', useFactory: getBaseUrl }
+        { provide: 'BASE_URL', useFactory: getBaseUrl },
+        {
+            // The server provides these in main.server
+            provide: REQUEST,
+            useFactory: (getRequest)
+        }
     ]
 })
 export class AppModule {
