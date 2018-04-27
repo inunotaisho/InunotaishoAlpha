0ee64398e64197e5d68e5f7fd9d9bf4d2d0e1480
inunotaisho26 <ebfairweather@gmail.com>
2018-04-27 14:07:15 -0400

    changing selector and adding to app.component

--- InunotaishoBeta/ClientApp/src/app/app.component.ts
+++ InunotaishoBeta/ClientApp/src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Inject, ViewEncapsulation, RendererFactory2, PLATFORM_ID, Injector } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute, PRIMARY_OUTLET } from '@angular/router';
+import { Meta, Title, DOCUMENT, MetaDefinition } from '@angular/platform-browser';
+import { Subscription } from 'rxjs/Subscription';
+import { isPlatformServer } from '@angular/common';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 }
